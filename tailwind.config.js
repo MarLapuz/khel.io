a1dc2b56fd18679eb8212c890aa082c3cf54af16
MarLapuz <lapuzmar@gmail.com>
2023-04-12 09:53:11 +0800

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true, // Strip hover styles for mobile devices.
+  },
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '6rem',
+        '2xl': '10rem',
+      },
+    },fontSize: {
+      xs: '0.75rem', // 12px
+      sm: '0.875rem', // 14px
+      base: '1rem', // 16px
+      lg: '1.125rem', // 18px
+      xl: '1.25rem', // 20px
+      '2xl': '1.375rem', // 22px
+      '3xl': '1.5rem', // 24px
+      '4xl': '1.625rem', // 26px
+      '5xl': '1.875rem', // 30px
+      '6xl': '2rem', // 32px
+      '7xl': '2.25rem', // 36px
+      '8xl': '2.5rem', // 40px
+      '9xl': '3rem', // 48px
+      '10xl': '3.75rem', // 60px
+      '11xl': '4.5rem', // 72px
+      '12xl': '6rem', // 96px
+      '13xl': '8rem', // 128px
+    },
     extend: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+        '3xl': '1920px',
+        '4xl': '2560px',
+        '5xl': '3840px',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -14,5 +55,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 }
